3ffb0101625ee8a8d8eb9fc944bdf8a01b7dd4b6
Paulo <paulinho-psp@hotmail.com>
2023-02-09 17:50:58 -0300

    Requisito 1 Finalizado

---
+++ src/pages/Login.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Login extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, () => this.validate());
+  };
+
+  validate = () => {
+    const { email, name } = this.state;
+
+    let validaEmail = false;
+
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (email.match(regex)) {
+      validaEmail = true;
+    } else {
+      validaEmail = false;
+    }
+
+    const min = 3;
+    const validaName = name.length >= min;
+
+    const dataValidate = (validaEmail && validaName);
+
+    this.setState({
+      isDisabled: !dataValidate,
+    });
+
+    return dataValidate;
+  };
+
+  render() {
+    const { email, name, isDisabled } = this.state;
+    return (
+      <div>
+        <form>
+          <input
+            name="email"
+            value={ email }
+            type="email"
+            onChange={ this.handleChange }
+            data-testid="input-gravatar-email"
+          />
+          <input
+            name="name"
+            value={ name }
+            type="text"
+            onChange={ this.handleChange }
+            data-testid="input-player-name"
+          />
+          <button
+            type="button"
+            disabled={ isDisabled }
+            data-testid="btn-play"
+          >
+            Play
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect()(Login);
