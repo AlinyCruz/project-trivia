86f799df4a7d97ed1b1e0ef62350762c2d58ec27
Alvaro-Bettoni <alvarobettoni2@gmail.com>
2023-02-10 16:07:14 -0300

    testa subir branch inacabada

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import md5 from 'crypto-js/md5'
+import md5 from 'crypto-js/md5';
+// testa subir branch
 
 class Header extends React.Component {
   render() {
