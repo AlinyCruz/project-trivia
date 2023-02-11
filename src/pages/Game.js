5763ae03a711a5e3c47887a0c739025973c54f91
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-02-11 11:08:16 -0300

    Requisito 8 em andamento

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import MainGame from '../components/MainGame';
 
 class Game extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Game extends React.Component {
       <div>
         <Header />
         <h2>Você esta na pagina do Jogo!!!</h2>
+        <MainGame />
       </div>
     );
   }
