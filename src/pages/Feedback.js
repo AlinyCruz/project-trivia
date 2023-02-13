c243f721b9ade7f9fbfc55251cadbd0980eb20ef
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-02-13 18:02:45 -0300

    Requisito 4 ajustes

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,9 +4,7 @@ import { connect } from 'react-redux';
 class Feedback extends React.Component {
   render() {
     return (
-      <h2
-        data-testid="feedback-text"
-      >
+      <h2 data-testid="feedback-text">
         Você esta na pagina de Feedback!!!
       </h2>
     );
